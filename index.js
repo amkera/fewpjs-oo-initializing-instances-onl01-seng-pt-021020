ed0d4616b364babfc2d658d5d419f8e3e77bfeac
Amber Kerawala Torres <amber_kerawala@berkeley.edu>
2020-09-08 02:20:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 // Write your code here
 class Breakfast {
+  constructor(name) {
+    
+  }
   
 }
 
 class Lunch {
+  constructor(name) {
+    
+  }
   
 }
 
 class Dinner {
+  constructor(name) {
+    
+  }
   
 }
 
